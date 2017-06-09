4e2d7594336f887c0fef6bdc80054f87c5271aa9
Radhika S <radhika@duretechnologies.com>
2017-06-09 12:21:55 +0530

    new changes

--- js/controller.js
+++ js/controller.js
@@ -24,3 +24,19 @@ function storeController($scope, $routeParams, DataService) {
         $scope.product = $scope.store.getProduct($routeParams.productSku);
     }
 }
+
+    function login() {
+            vm.dataLoading = true;
+            AuthenticationService.Login(vm.username, vm.password, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(vm.username, vm.password);
+                    $location.path('/');
+                } else {
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+    };
+
+
+
